6a2d3509ccfcfed451022d60b64d74e73c0a46a2
cgrzmustafa <cigersizmustafa5@gmail.com>
2025-08-21 16:42:27 +0300

    ...

--- CSharpEgitimKampi601/FrmEmployee.Designer.cs
+++ CSharpEgitimKampi601/FrmEmployee.Designer.cs
@@ -161,6 +161,7 @@
             this.btnCreate.TabIndex = 47;
             this.btnCreate.Text = "Ekle";
             this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnList
             // 
@@ -213,6 +214,7 @@
             this.Controls.Add(this.label1);
             this.Name = "FrmEmployee";
             this.Text = "FrmEmployee";
+            this.Load += new System.EventHandler(this.FrmEmployee_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
